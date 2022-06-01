2d8afd556170d9cf5c29840f5d20f9e3f4511640
Jerry Wang <jerrygw2004@gmail.com>
2022-05-31 20:28:29 -0400

    week two hw

---
+++ homework_2,3.py
@@ -0,0 +1,4 @@
+hrs = float(input('How many hours have you worked? '))
+rate = float(input('How much do you make per hour? '))
+totalP = hrs*rate
+print(f'Your pay is $ {totalP}')
---
+++ homework_2,4.py
@@ -0,0 +1,6 @@
+width = 17
+height = 12.0
+print(f'{width//2} {type(width//2)}')
+print(f'{width/2.0} {type(width/2.0)}')
+print(f'{height/3} {type(height/3)}')
+print(f'{1+2*5} {type(1+2*5)}')
