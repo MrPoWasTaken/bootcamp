fbaca2c3eb931e70a5b7ede77c9b55a42922fcaf
Jerry Wang <jerrygw2004@gmail.com>
2022-06-23 12:14:42 -0400

    weak 4 hw

---
+++ homework_4.1.py
@@ -0,0 +1,29 @@
+numbers = []
+largest = None
+smallest = None
+user= None
+
+
+while True:
+  try:
+    user = input('Enter a number: ')
+    user = float(user)
+    numbers.append(user)
+
+  except:
+    if user == "stop":
+      break
+    print('Please enter a number')
+
+for i in numbers:
+  if largest == None:
+    largest = i
+    smallest = i
+
+  if i > largest:
+    largest = i
+  if i < smallest:
+    smallest = i
+
+print(f'The largest number you said was {largest}')
+print(f'The smallest number you said was {smallest}')
---
+++ homework_4.2.py
@@ -0,0 +1,5 @@
+text = "fnaengrinabrgioewfnrei: .52576"
+
+position = text.find(' ')
+numbers = text[position+1:]
+print (float(numbers))
